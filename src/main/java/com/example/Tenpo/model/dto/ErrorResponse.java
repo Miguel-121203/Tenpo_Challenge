23850d752177ac46c1ef44d2b36595c233f3f439
Miguel Angel <miguelangel.br1203@gmail.com>
2026-01-28 19:33:18 -0500

    add: Creacion de entidad y dtos de respuesta y error

---
+++ src/main/java/com/example/Tenpo/model/dto/ErrorResponse.java
@@ -0,0 +1,36 @@
+package com.example.Tenpo.model.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Registro del historial de llamadas")
+public class ErrorResponse {
+
+    @Schema(description = "ID del registro")
+    private Long id;
+
+    @Schema(description = "Fecha y hora de la llamada")
+    private LocalDateTime timestamp;
+
+    @Schema(description = "Endpoint invocado")
+    private String endpoint;
+
+    @Schema(description = "Parámetros de la llamada en formato JSON")
+    private String parameters;
+
+    @Schema(description = "Respuesta de la llamada en formato JSON")
+    private String response;
+
+    @Schema(description = "Error si la llamada falló")
+    private String error;
+}
+
---
+++ src/main/java/com/example/Tenpo/model/entity/CallHistory.java
@@ -0,0 +1,36 @@
+package com.example.Tenpo.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "call_history")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CallHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+
+    @Column(nullable = false)
+    private String endpoint;
+
+    @Column(columnDefinition = "TEXT")
+    private String parameters;
+
+    @Column(columnDefinition = "TEXT")
+    private String response;
+
+    @Column(columnDefinition = "TEXT")
+    private String error;
+}
