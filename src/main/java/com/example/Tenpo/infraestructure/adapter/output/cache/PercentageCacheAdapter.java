121f8583111b3a3cba877a852c849818c2ed1fa5
Miguel Angel <miguelangel.br1203@gmail.com>
2026-01-29 11:58:04 -0500

    add: adaptador de cache y de servicio externo (mock) listo

---
+++ src/main/java/com/example/Tenpo/infraestructure/adapter/output/cache/PercentageCacheAdapter.java
@@ -0,0 +1,45 @@
+package com.example.Tenpo.infraestructure.adapter.output.cache;
+
+import com.example.Tenpo.domain.port.output.PercentageCachePort;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PercentageCacheAdapter implements PercentageCachePort {
+
+    private static final String CACHE_NAME = "percentageCache";
+    private static final String CACHE_KEY = "current";
+
+    private final CacheManager cacheManager;
+
+    @Override
+    public Optional<Double> get() {
+        Cache cache = cacheManager.getCache(CACHE_NAME);
+        if (cache != null) {
+            Double value = cache.get(CACHE_KEY, Double.class);
+            if (value != null) {
+                log.info("Valor encontrado en cache: {}",value);
+                return Optional.of(value);
+            }
+        }
+        log.info("No se encontro el valor del cache");
+        return Optional.empty();
+    }
+
+    @Override
+    public void save(Double percentage) {
+        Cache cache = cacheManager.getCache(CACHE_NAME);
+        if (cache != null) {
+            cache.put(CACHE_KEY, percentage);
+            log.info("Valor guardado en cache: {}",percentage);
+        }
+
+    }
+}
--- src/main/java/com/example/Tenpo/infraestructure/adapter/output/external/ExternalPercentageAdapter.java
+++ src/main/java/com/example/Tenpo/infraestructure/adapter/output/external/ExternalPercentageAdapter.java
@@ -1,31 +1,44 @@
 package com.example.Tenpo.infraestructure.adapter.output.external;
 
-import lombok.Value;
+import com.example.Tenpo.domain.exception.ExternalServiceException;
+import com.example.Tenpo.domain.port.output.PercentageProviderPort;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
 @Slf4j
 @Component
-public class ExternalPercentageAdapter {
+public class ExternalPercentageAdapter implements PercentageProviderPort {
     private final double percentageValue;
-    private final double failtureRate;
+    private final double failureRate;
     private final Random random;
 
     public ExternalPercentageAdapter(
-            @Value("${external.percentage.value}") double percentageValue,
-            @Value("${external.percentage.failture-rate}") double failtureRate){
+            @org.springframework.beans.factory.annotation.Value("${external.percentage.value}") double percentageValue,
+            @Value("${external.percentage.failure-rate}") double failureRate) {
         this.percentageValue = percentageValue;
-        this.failtureRate = failtureRate;
+        this.failureRate = failureRate;
         this.random = new Random();
     }
 
+    @Override
+    public Double getPercentage() {
+        log.info("Llamando al servicio externo para obtener porcentaje");
 
+        simulateNetworkDelay();
 
+        if (shouldFail()){
+            log.warn("Servicio externo fallo (Simulacion)");
+            throw new ExternalServiceException("Servicio externo no esta disponible");
+        }
+
+        log.info("Servicio externo respondio con porcentaje {}", percentageValue);
+        return percentageValue;
+    }
 
     private void simulateNetworkDelay(){
-
         try{
             Thread.sleep(100+ random.nextInt(200));
         }catch (InterruptedException e){
@@ -33,6 +46,7 @@ public class ExternalPercentageAdapter {
         }
     }
 
-    private boolean shouldFail(){return random.nextDouble() < failtureRate;}
+    private boolean shouldFail(){return random.nextDouble() < failureRate;}
+
 
 }
